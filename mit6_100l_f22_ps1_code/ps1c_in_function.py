42a8a743b6eae9c86329b4d0f80f7e6fe9fa5b2d
cooper518 <coophan11@gmail.com>
2026-07-20 20:41:23 -0500

    ps1 done

--- mit6_100l_f22_ps1_code/ps1c_in_function.py
+++ mit6_100l_f22_ps1_code/ps1c_in_function.py
@@ -1,9 +1,27 @@
 def part_c(initial_deposit):
 	#########################################################################
-	
-	
+	amount_saved = 0.0
+	high = 1.0
+	low = 0.0
+	r = 0.5
+	amount_needed = 200_000.0
+	months = 36
+	epsilon = 100.0
+	steps = 0
 	##################################################################################################
 	## Determine the lowest rate of return needed to get the down payment for your dream home below ##
 	##################################################################################################
-	
+	while abs(amount_needed-amount_saved) > epsilon:
+		amount_saved = initial_deposit*(1+(r/12))**months
+		if r>0.99:
+			r = None
+			break
+		if amount_saved < amount_needed:
+			low = r
+		else:
+			high = r
+		r = (high+low)/2.0
+		steps += 1
+	print(f'Best savings rate: {r}')
+	print(f'Steps in bisection search: {steps}')
 	return r, steps
